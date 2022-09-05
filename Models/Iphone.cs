1125516327aa61eaa21b94ca5e9ef05bfa5e1f73
Anderson Souza <estudo@estudo.com>
2022-09-05 12:42:45 -0300

    Desafio concluído, desafio POO!

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,18 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    public class Iphone : Smartphone
     {
+        //Implementado
+        public Iphone(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
+        {
+        }
+
         // TODO: Sobrescrever o método "InstalarAplicativo"
+        //Implementado
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            System.Console.WriteLine($"Instalação aplicativo do {nomeApp} foi concuída " );
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+System.Console.WriteLine("Iniciando aplicação Nokia");
+Smartphone nokia = new Nokia(numero:"+55(11)11111-1111", modelo:"N-AZ1234", imei:"111111111111", memoria: 64);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Whatsapp");
+
+System.Console.WriteLine("iniciando aplicação Iphone");
+Smartphone iphone = new Iphone(numero: "+55()22222-2222", modelo: "A-IP1234", imei: "222222222222", memoria: 128);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Itune");
